e7baab341d5e9237ef087eddb375d05766a8829f
Matthew Greenspun <matthewgreenspun@gmail.com>
2021-04-18 21:28:42 -0400

    backend can get the courses JSON object using the classroom.courses.list method

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 require("dotenv").config();
 import express from "express";
 import { google } from "googleapis";
+import axios from "axios";
 const cors = require("cors");
 
 const listCoursesUrl = "https://classroom.googleapis.com/v1/courses";
-const SCOPES = ["https://www.googleapis.com/auth/classroom.courses.readonly"];
+const SCOPES = [
+  "https://www.googleapis.com/auth/classroom.courses",
+  "https://www.googleapis.com/auth/classroom.courses.readonly",
+];
+enum CourseState {
+  COURSE_STATE_UNSPECIFIED,
+  ACTIVE,
+  ARCHIVED,
+  PROVISIONED,
+  DECLINED,
+}
 
 const app = express();
 app.use(cors());
@@ -32,18 +43,28 @@ app.get("/courses", async (req, res) => {
     process.env.CLIENT_SECRET,
     "http://localhost:3000/courses"
   );
-  const token = await oAuth2Client.getToken(code as string);
 
-  res.send(`
-    <h1>Your Courses: </h1>
-    <p>code: ${code}</p>
-    <p>token: ${JSON.stringify(token)}</p>
+  const { tokens } = await oAuth2Client.getToken(code as string);
+  oAuth2Client.setCredentials(tokens);
+  const classroom = google.classroom({ version: "v1", auth: oAuth2Client });
+  try {
+    const response = await classroom.courses.list({});
+    const courses = response.data;
+    res.send(`
+      <a href="/">home</a>
+      <h1>Your Courses: </h1>
+      <pre>${JSON.stringify(courses)}</pre>
+    `);
+  } catch (err) {
+    console.error(err);
+    res.send(`
+      <a href="/">home</a>
+      <h1>Your Courses: </h1>
+      <p>Failed to get your courses. Please try again.</p>
   `);
+  }
 });
 
 app.listen(process.env.PORT, () =>
-  console.log(
-    "Google Classroom Automation listening on port ",
-    process.env.PORT
-  )
+  console.log("Google Classroom Automation listening on port", process.env.PORT)
 );
